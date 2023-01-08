01fe3db5a092353bf2ea7fb525591f8be91a0895
Алексей Зорин <zorinalexey59292@gmail.com>
2023-01-09 01:25:49 +0400

    first commit

---
+++ src/Blocks/Head.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Liberty\HtmlBuilder\Blocks;
+
+/**
+ * Класс Head
+ * @version 0.0.1
+ * @package Liberty\HtmlBuilder\Blocks
+ * @generated Зорин Алексей, please DO NOT EDIT!
+ * @author Зорин Алексей <zorinalexey59292@gmail.com>
+ * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
+ */
+final class Head
+{
+
+}
--- src/HTMLv5.php
+++ src/HTMLv5.php
@@ -4,6 +4,13 @@ declare(strict_types = 1);
 
 namespace Liberty\HtmlBuilder;
 
+use \Liberty\HtmlBuilder\Blocks\Head;
+use \Liberty\HtmlBuilder\Blocks\Body;
+use \Liberty\HtmlBuilder\Attributes\Manifest;
+use \Liberty\HtmlBuilder\Attributes\Lang;
+use \Liberty\HtmlBuilder\Attributes\Universal AS Attributes;
+use \Liberty\HtmlBuilder\Tags\AbstractTag;
+
 /**
  * Класс HTMLv5
  * @version 0.0.1
@@ -12,7 +19,41 @@ namespace Liberty\HtmlBuilder;
  * @author Зорин Алексей <zorinalexey59292@gmail.com>
  * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
  */
-final class HTMLv5
+final class HTMLv5 extends AbstractTag
 {
+
+    use Attributes,
+        Lang,
+        Manifest;
+
+    private ?Head $head;
+    private ?Body $body;
+
+    public function head(): Head
+    {
+        if ( ! $this->head) {
+            $this->head = new Head();
+        }
+        return $this->head;
+    }
+
+    public function body(): Body
+    {
+        if ( ! $this->body) {
+            $this->body = new Body();
+        }
+        return $this->body;
+    }
+
+    public function __toString()
+    {
+        $str = '<!DOCTYPE><html';
+        $str .= $this->setTagAttrubutes($this);
+        $str .= '>';
+        $str .= (string)$this->head;
+        $str .= (string)$this->body;
+        $str .= '</html>';
+        return $str;
+    }
 
 }
